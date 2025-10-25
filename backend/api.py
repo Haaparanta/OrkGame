e2dffef403563c1785408937f2dd331d5a1cf00f
Juha Ylikoski <juylikoski@gmail.com>
2025-10-25 17:42:26 +0300

    Add session concept, remove pyc files

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,29 @@
-from fastapi import FastAPI
+from fastapi import Cookie, Depends, FastAPI, HTTPException, Response, status
 
 
 app = FastAPI()
 
 
+def get_game_session(
+    game_session: str | None = Cookie(default=None, alias="game-session"),
+):
+    """Dependency that retrieves the 'game-session' cookie value.
+
+    Raises 401 if missing.
+    """
+    if game_session is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing 'game-session' cookie",
+        )
+    return game_session
+
+
 @app.get("/")
-def hello():
-    return "hi"
+def hello(session_id: str = Depends(get_game_session)):
+    return session_id
+
+
+@app.post("/attach-session")
+def attach_session(response: Response, session_name: str):
+    response.set_cookie(key="game-session", value=session_name)
